bf5c0fa9f316f749dd44ac503d1ff4eeadb164b3
Rony Lev-Ari <ronylevari@gmail.com>
2017-07-17 21:54:37 +0300

    added game board data structure and extracted the game difficulty data structure to a seperate file

---
+++ iSmack-a-frog/GameBoardSize.swift
@@ -0,0 +1,19 @@
+//
+//  GameBoardSize.swift
+//  iSmack-a-frog
+//
+//  Created by rony_temp on 17/07/2017.
+//  Copyright © 2017 rony_temp. All rights reserved.
+//
+
+import Foundation
+
+struct GameBoardSize {
+    let rows: Int
+    let columns: Int
+    
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+    }
+}
---
+++ iSmack-a-frog/GameDifficulty.swift
@@ -0,0 +1,15 @@
+//
+//  GameDifficulty.swift
+//  iSmack-a-frog
+//
+//  Created by rony_temp on 17/07/2017.
+//  Copyright © 2017 rony_temp. All rights reserved.
+//
+
+import Foundation
+
+enum GameDifficulty: String {
+    case Easy
+    case Normal
+    case Hard
+}
