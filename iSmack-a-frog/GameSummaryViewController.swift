920e9b37405a7f480ad67775cd549434fd1ff4bb
Rony Lev-Ari <ronylevari@gmail.com>
2017-09-19 01:28:49 +0300

    started working on UI of the winner list and the game summary announcement UI

--- iSmack-a-frog/GameSummaryViewController.swift
+++ iSmack-a-frog/GameSummaryViewController.swift
@@ -15,6 +15,12 @@ class GameSummaryViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var earthImage: UIImageView!
+    
+    @IBOutlet weak var mapTxet: UILabel!
+    
     @IBOutlet weak var pointLabel: UILabel!
     
     var points: Int!
@@ -25,6 +31,22 @@ class GameSummaryViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapTxet.isUserInteractionEnabled = true
+        earthImage.isUserInteractionEnabled = true
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(loadWinnerMap(tapGestureRecognizer:)))
+        
+        mapTxet.addGestureRecognizer(tapGesture)
+        earthImage.addGestureRecognizer(tapGesture)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.isScrollEnabled = false
+        tableView.separatorColor = UIColor.clear
+        // tableView.isHidden = true
+        
         pointLabel.text = String(points) + " points"
         if dataAccessObject.isAmongWinners(with: points) {
             let player = Player(score: points, name: "pipi2", latitude: 0.0, longitude: 0.0)
@@ -35,5 +57,29 @@ class GameSummaryViewController: UIViewController {
             print(winners[i])
         }
         
+    }
+    
+    func loadWinnerMap(tapGestureRecognizer: UITapGestureRecognizer) {
+        print("opening map")
+    }
+}
+
+extension GameSummaryViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = winners[0].description
+        return cell
+    }
+}
+
+extension GameSummaryViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
 }
